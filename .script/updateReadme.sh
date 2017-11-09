8bbcfa59a6cddb6330c16e66034b1795115aa6a0
yajamon <yajamon.tatsuki@gmail.com>
2017-11-09 12:12:02 +0900

    Edit script: update README
    put information
    use cat intead to echo lines

--- .script/updateReadme.sh
+++ .script/updateReadme.sh
@@ -2,7 +2,15 @@
 
 readonly ROOT=$( dirname $(dirname $(realpath $0) ) )
 readonly TARGET=$ROOT/README.md
-readonly HEADER=$(cat << EOT
+
+put_info() {
+    echo "[$(date +%Y-%m-%d.%H:%M:%S.%N)] $1" >&2
+}
+
+put_info "start update README.md"
+
+put_info "write header"
+cat << EOT > $TARGET
 # TIL
 
 Today I Learned
@@ -10,44 +18,49 @@ Today I Learned
 素晴らしき書捨て
 
 - - -
-EOT
-)
 
+EOT
+
+
+put_info "pick categories"
 readonly CATEGORIES=$(find $ROOT -mindepth 1 -maxdepth 1 -name "[a-zA-Z]*" -type d)
 
-{
-    echo "$HEADER"
-    echo ""
-} > $TARGET
+put_info "start write categories TOC"
+cat << EOT >> $TARGET
+## Categories
 
-{
-    echo "## Categories"
-    echo ""
-} >> $TARGET
+EOT
 
 for p in $CATEGORIES
 do
     category_name=$(basename $p)
+    put_info "write category: $category_name"
     echo "- [${category_name}](#${category_name})" >> $TARGET
 done
 
-{
-    echo ""
-    echo "- - -"
-    echo ""
-} >> $TARGET
+cat << EOT >> $TARGET
+
+- - -
+
+EOT
+
+put_info "write content TOC"
 
 for p in $CATEGORIES
 do
     category_name=$(basename $p)
+    put_info "write category: $category_name"
     echo "### ${category_name^}" >> $TARGET
     echo "" >> $TARGET
 
     for f in $(find $p -type f)
     do
         title=$(head -1 $f)
+        put_info "write content: $title"
         echo "- [${title#\# *}](${f#$ROOT/})" >> $TARGET
     done
 
     echo "" >> $TARGET
 done
+
+put_info "complete"
